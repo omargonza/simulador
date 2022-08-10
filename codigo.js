5bc11fbb1140cb2040fcf58fdd6f5b4a4337e235
omargonza <gonzamartinez1081@gmail.com>
2022-08-10 09:06:42 -0300

    cambios en calculo de moneda

--- codigo.js
+++ codigo.js
@@ -1,82 +1,4 @@
-/*const COTIZACION_DOLAR = 140;
-const COTIZACION_DOLAR_ILEGAL = 280;
-const COTIZACION_DOLAR_TURISTA = 240;
-const cotizarDolar =(pesos) => pesos / COTIZACION_DOLAR;
-const cotizarPesos =(dolar) => dolar * COTIZACION_DOLAR;
-let seleccion = prompt("SELECCIONAR COTIZACION \n 1 - DOLARESS A PESOS \n 2 - PESOS A DOLAR");
-let valor = prompt("VALOR");
-switch(seleccion){
-    case "1":
-         alert(cotizarPesos(valor));
-         break;
 
-    case "2":
-         alert (cotizarDolar(valor));
-         break;
-    default:
-         break;     
-         const DOLARES = [{id:1, COTIZACION_DOLAR_ILEGAL:280},
-                            {id:2, COTIZACION_DOLAR:140},
-                             {id:3, COTIZACION_DOLAR_TURISTA:}];
-
-
-
-         for (const DOLARES of DOLARES) {
-          console.log(DOLARES.id);
-          console.log(DOLARES.DOLARES);
-         }        
-         
-    
-
-*//*
-
-const COTIZACION_DOLAR = 140;
-const COTIZACION_DOLAR_ILEGAL = 280;
-const COTIZACION_DOLAR_TURISTA = 240;
-const cotizarDolar =(pesos) => pesos / COTIZACION_DOLAR;
-const cotizarPesos =(dolar) => dolar * COTIZACION_DOLAR;
-let seleccion = prompt("SELECCIONAR COTIZACION \n 1 - DOLARESS A PESOS \n 2 - PESOS A DOLAR");
-let valor = prompt("VALOR");
-switch(seleccion){
-    case "1":
-         alert(cotizarPesos(valor));
-         break;
-    case "2":
-         alert (cotizarDolar(valor));
-         break;
-    default:
-         break;     
-        
-
-         
-}
-const DOLARES = [{tipo:"blue",valor:280},
-{tipo:"oficial",valor:140},
-{tipo:"turista",valor:240}];
-for (const dolar of DOLARES) {
-console.log(dolar.valor);
- console.log(dolar.DOLARES);
-}     */  /*
-
-
-
-
-function convertir(dolarOficial, dolarBlue, dolarTurista) {
-     this.dolarOficial=dolarOficial;
-     this.dolarBlue=dolarBlue;
-     this.dolarTurista=dolarTurista;
-     let valore = parseInt(document.getElementById("valor").value);
-     let resultado =0;
-     let dolarOficial =140;
-     let dolarBlue =290;
-     let dolarTurista =240;
-     if(document.getElementById("uno, dos, tres").Checked){
-          resultado = valore / convertir;
-          alert("El cambio de Pesos a Dolar oficial es: $" + resultado)
-     }
-}
-
-*/
 
 class Dolar {
      constructor(tipo, valor) {
@@ -101,22 +23,22 @@ function convertir(valor) {
           console.log(arrayValores[0])
           resultado = valor / arrayValores[0];
           console.log(resultado)
-          alert("el cambio a Dolar oficial es: $" + resultado.toFixed(2));
+          alert("Valor del Dolar Oficial 140, el cambio de moneda es: $" + resultado.toFixed(2));
 
      }
      if (document.getElementById('dos').checked)
       {
           resultado = valor / arrayValores[1];
           console.log(resultado)
-          alert("el cambio a Dolar Blue es: $" + resultado.toFixed(2));
+          alert("Valor del Dolar Blue es 290, el cambio de moneda es: $" + resultado.toFixed(2));
 
      }
      if (document.getElementById('tres').checked) {
           resultado = valor / arrayValores[2];
           console.log(resultado)
-          alert("el cambio a Dolar Turista es: $" + resultado.toFixed(2));
+          alert("Valor del Dolar Turista es 240, el cambio de moneda es: $" + resultado.toFixed(2));
      } else {
-          alert("introduzca un valor");
+          alert("GRACIAS POR ELEGIRNOS!!");
      }
 
 }
